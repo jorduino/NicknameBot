2a8f9415d1b864eac108da96d0a4f60c0008f630
jorduino <jordanhabash@gmail.com>
2026-01-09 01:15:10 -0500

    feat: created index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Discord from 'discord.js';
+import chalk from 'chalk';
+
+console.clear();
+console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), (chalk.green(`Starting up`)), (chalk.white(`...`)))
+console.log(`\u001b[0m`)
+console.log(chalk.red(`© Jorduino | 2025 - ${new Date().getFullYear()}`))
+console.log(chalk.red(`All rights reserved`))
+console.log(`\u001b[0m`)
+console.log(`\u001b[0m`)
+console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), chalk.red(`Version ${require(`${process.cwd()}/package.json`).version}`), (chalk.green(`loaded`)))
+console.log(`\u001b[0m`);
+console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), (chalk.green(`Shutting Down`)), (chalk.white(`... Goodbye!`)))
